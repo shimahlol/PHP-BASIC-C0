e438c8e1fa759052da93359be5850f54c12735ca
jamirolol <2048415@talnet.nl>
2021-12-23 12:48:26 +0100

    arsort

---
+++ 3-Arrays/3.5 - Array sorteren/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$laag_hoog =[676,5345,78657,3,46,21,47];
+asort($laag_hoog);
+print_r ($laag_hoog);
+echo "<br>";
+$hoog_laag = [676,5345,78657,3,46,21,47];
+arsort($hoog_laag);
+print_r ($hoog_laag);
