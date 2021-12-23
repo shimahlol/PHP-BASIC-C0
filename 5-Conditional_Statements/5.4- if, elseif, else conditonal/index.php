f74d96cfa9419435fca093bc36023c4b5d1e4e64
jamirolol <2048415@talnet.nl>
2021-12-23 15:04:32 +0100

    prod_besteld

---
+++ 5-Conditional_Statements/5.4- if, elseif, else conditonal/index.php
@@ -0,0 +1,33 @@
+<?php
+$prod_besteld = 12;
+
+if($prod_besteld < 10) {
+    $prijs = 1.50;
+} elseif($prod_besteld < 20) {
+    $prijs = 1.25;
+} else {
+    $prijs = 1.00;
+}
+
+echo $prijs;
+$prod_besteld = 9;
+
+if($prod_besteld < 10) {
+    $prijs = 1.50;
+} elseif($prod_besteld < 20) {
+    $prijs = 1.25;
+} else {
+    $prijs = 1.00;
+}
+
+echo $prijs;
+$prod_besteld = 24;
+
+if($prod_besteld < 10) {
+    $prijs = 1.50;
+} elseif($prod_besteld < 20) {
+    $prijs = 1.25;
+} else {
+    $prijs = 1.00;
+}
+echo $prijs;
