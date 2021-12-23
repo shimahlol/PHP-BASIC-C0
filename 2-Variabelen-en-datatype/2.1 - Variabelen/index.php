117531c6acee070ad9d882ee500f570704db0bf9
jamirolol <2048415@talnet.nl>
2021-12-23 14:42:48 +0100

    muchos changes

--- 2-Variabelen-en-datatype/2.1 - Variabelen/index.php
+++ 2-Variabelen-en-datatype/2.1 - Variabelen/index.php
@@ -1,11 +1,11 @@
-<!DOCTYPE html>
-<html>
-<body>
-
 <?php
-$score = 0;
-echo $score;
-?>
-
-</body>
-</html> 
+$myHeroesNames =  ['tijger', 'mickey', 'tijger'];
+echo $myHeroesNames[0];
+echo $myHeroesNames[1];
+echo $myHeroesNames[2];
+$myHeroesNames[1] = 'panter';
+echo $myHeroesNames[1];
+echo "<br>";
+$myHeroesNames[3] = "jamiro";
+array_push($myHeroesNames, "silver");
+print_r($myHeroesNames); 
--- 5-Conditional_Statements/5.3 - if, else conditional/index.php
+++ 5-Conditional_Statements/5.3 - if, else conditional/index.php
@@ -2,17 +2,37 @@
 $test1 = 10;
 $test2 = 6;
 if ($test1 == $test2) {
-	echo "gelijk";
-}
+	echo "gelijk";}
+	else{
+		echo "ongelijk";
+	}
 if ($test1 != $test2){
 	echo "ongelijk";}
+	else{
+		echo "gelijk";
+	}
 if ($test1 === $test2){
 	echo "identiek";}
+	else{
+		echo "niet identiek";
+	}
 if ($test1 > $test2){
 	echo "1groted dan2";}
+	else{
+		echo "2 is groter dan 1";
+	}
 if ($test1 < $test2){
 	echo "1kleiner dan";}
+	else{
+		echo "2 is kleiner dan 1";
+	}
 if ($test1 >= $test2){
 	echo "1gelijk of groter dan2";}
+	else{
+		"2is gelijk of groter dan 1";
+	}
 if ($test1 <= $test2){
 	echo "2gelijk of groter dan";}
+	else{
+		echo "1is gelijk of groter dan2";
+	}
