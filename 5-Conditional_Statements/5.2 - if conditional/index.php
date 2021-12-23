a770f8c4b093b6552c2d20661b4a53e30afee839
jamirolol <2048415@talnet.nl>
2021-12-23 14:42:36 +0100

    much change

---
+++ 5-Conditional_Statements/5.2 - if conditional/index.php
@@ -0,0 +1,18 @@
+<?php
+$test1 = 10;
+$test2 = 6;
+if ($test1 == $test2) {
+	echo "gelijk";
+}
+if $test1 != $test2;
+	echo "ongelijk";
+if $test1 === $test2;
+	echo "identiek";
+if $test1 > $test2;
+	echo "1groted dan2";
+if $test1 < $test2;
+	echo "1kleiner dan";
+if $test1 >= $test2;
+	echo "1gelijk of groter dan2";
+if $test1 <= $test2;
+	echo "2gelijk of groter dan";
