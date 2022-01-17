9d9c5de737c979ca227aa0d49355a5a9d4f3082a
jamirolol <2048415@talnet.nl>
2022-01-17 15:55:17 +0100

    &&

---
+++ 5-Conditional_Statements/5.6 - Logical Operators/index.php
@@ -0,0 +1,24 @@
+<?php
+$var1 = 4;
+$var2 = 9;
+
+if($var1 < 5 && $var1 > 10){
+	echo "hello";}
+else{
+	echo "goodbye";
+}
+
+if($var1 < 5 || $var1 > 0){
+	echo "hello";}
+else{
+	echo "goodbye";}
+
+if($var1 < 5 != $var1 > 10){
+	echo "hello";}
+else{
+	echo "goodbye";}
+
+if($var1 < 5 xor $var1 > 10){
+		echo "hello";}
+else{
+	echo "goodbye";}
