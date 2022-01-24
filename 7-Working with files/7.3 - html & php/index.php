f28c2b6ed2c8db533e69fa1b1fc13275fc2aa1cf
jamirolol <2048415@talnet.nl>
2022-01-24 13:48:56 +0100

    baby

---
+++ 7-Working with files/7.3 - html & php/index.php
@@ -0,0 +1,20 @@
+<?php
+	$leeftijd = 16;
+	?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>the duck!?</title>
+</head>
+<body>
+	<div id="isMeerderJarig">
+		<?php
+		if($leeftijd < 18 ){
+			echo "<h3> ga je mama's melk drinken kleintje</h3>";
+		}
+		?>
+</body>
+</html>
